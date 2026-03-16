937bfe4ff25ed0683e259de87e884696d2daacfc
Wenqi Cai <wenqicai.97@gmail.com>
2026-03-16 12:14:28 +0400

    Typo

--- ros2_ws/src/mujoco_g1/setup.py
+++ ros2_ws/src/mujoco_g1/setup.py
@@ -5,7 +5,7 @@ package_name = 'mujoco_g1'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
